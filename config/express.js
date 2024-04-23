582a8921462dbdcd0b6fb5becfcce9a42f55d44c
bankjirapan <jirapan_yankhan@hotmail.com>
2024-04-23 16:19:09 +0700

    [EDIT] Add session store

--- config/express.js
+++ config/express.js
@@ -1,35 +1,37 @@
-import express from 'express'
-import path from 'path'
+import express from "express";
+import path from "path";
+import session from 'express-session'
+import { v4 as uuidv4 } from "uuid";
 
+// Route
+import routes from "../routes/routes";
 
-
-const app = express()
-
-
+const app = express();
 
 const createApp = {
-    
-    setup: function () {
+  setup: function () {
+
+    app.use(session({ 
+        genid: function(req) {
+          return uuidv4()
+        },
+        saveUninitialized: true,
+        resave: false,
+        secret: 'keyboard cat'
+      }))
+
+    // View
+    app.set("view engine", "pug");
+    app.set("views", "views");
+    app.use(express.static(path.join(__dirname, "../public")));
+
+    app.use(routes);
 
 
-        // View
-        app.set('view engine', 'pug');
-        app.set('views', 'views');
-        app.use(express.static(path.join(__dirname, '../public')));
-
-
-        app.get("/",(req,res,next) => {
-            res.render('index')
-        })
-
-        app.listen(3005,() => {
-            console.log('App started at port 3005')
-        })
-
-
-
-    }
-
-}
+    app.listen(8005, () => {
+      console.log("App started at port 8005");
+    });
+  },
+};
 
 export default createApp;
