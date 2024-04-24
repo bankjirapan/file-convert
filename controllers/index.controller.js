1426dc0288bf795fd0020de80a6988b1316f508b
bankjirapan <jirapan_yankhan@hotmail.com>
2024-04-24 22:49:35 +0700

    [EDIT] Create session when into index page

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,10 +1,13 @@
+import { v4 as uuidv4 } from "uuid";
 
 const indexCtrl = {
   getIndex: (req, res, next) => {
+    req.session.process = uuidv4()
     res.render("index", {
+      process: req.session.process
     });
+
   },
 };
-
 
 export default indexCtrl;
